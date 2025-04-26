d23863165bab2dde66a10ceec57f3dc303c41d69
LuisMdz <luis3mendez6@gmail.com>
2025-04-25 23:53:17 -0600

    Added MQTT sender

--- py_reader/get_data.py
+++ py_reader/get_data.py
@@ -5,17 +5,24 @@ from time import sleep
 import datetime as dt
 import bme280
 import smbus2
+import paho.mqtt.client as mqtt
 from gpiozero import CPUTemperature
+from gpiozero.pins.native import NativeFactory
+from gpiozero import Device
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 
 def get_cpu_temp():
     """Reading temperature from CPU"""
-
-    cpu = CPUTemperature()
-    print(f"Current temperature: {cpu.temperature}")
-    return round(float(cpu.temperature), 2)
+    cpu = None
+    try:
+        cpu = CPUTemperature()
+        print(f"Current temperature: {cpu.temperature}")
+        cpu = round(float(cpu.temperature), 2)
+    except Exception as e:
+        print(f"Error: {e.args}")
+    return cpu
 
 
 def read_sensor():
@@ -63,7 +70,6 @@ def write_data(data: dict):
     # write line protocol
     # measurement: weather, location = Office
     # field tags: Temperature, Pressure, Humidity
-    #
     weather = influxdb_client.Point("weather").tag(
         "location", "office").field(
             "temperature", data["temperature"]).field(
@@ -75,12 +81,64 @@ def write_data(data: dict):
     write_api.write(bucket=bucket, org=org, record=weather)
 
 
+def load_topics(selector=None):
+    """Return topic selected"""
+
+    # build topics for each sensor
+    _topics = None
+    if selector is None:
+        _topic = "rp1/weather"
+        _names = ["temperature", "humidity", "pressure"]
+        _topics = {_name: {"topic": _top} for _name,
+                   _top in zip(_names, [f"{_topic}/{x}" for x in _names])}
+    return _topics
+
+
+def create_mqtt_client():
+    """Create MQTT client to connection"""
+
+    BROKER = "192.168.68.109"
+    PORT = 1883
+    username = "mqtt_usr"
+    password = "luismdz366"
+    client = mqtt.Client()
+    client.username_pw_set(username, password)
+    client.connect(BROKER, PORT, 60)
+    return client
+
+
+def pub_mqtt(client, data):
+    """Publish MQTT"""
+
+    _topics = load_topics()
+    try:
+        cputemp = get_cpu_temp()
+        if cputemp:
+            client.publish("rp2/system/cpu_temp", cputemp)
+        print(f"PUB: rp2/system/cpu_temp - {cputemp}")
+        for sensor, value in data.items():
+            client.publish(_topics.get(sensor)["topic"], value)
+            print(
+                f"Pub topic: {_topics.get(sensor)["topic"]}, val: {value}")
+        sleep(5)
+    except Exception as e:
+        print(f"ERROR: {e.args}")
+    finally:
+        client.disconnect()
+
+
 if __name__ == "__main__":
 
+    Device.pin_factory = NativeFactory()
+    _client = create_mqtt_client()
     while True:
         try:
+            # read sensor BME280
             data_point = read_sensor()
+            # write data into influxdb
             write_data(data_point)
+            # publish data to MQTT
+            pub_mqtt(_client, data_point)
             sleep(5)
         except Exception as e:
             print(f"Exception has occured {e.args}")
